efe59de6c2d9b898c67df282d8471e03b83e0b25
Anil Adhikari <adhikarianil553@gmail.com>
2025-11-23 17:18:52 +0545

    Add my R file

---
+++ rs.R
@@ -0,0 +1,13 @@
+print("Hello World")
+a<-5
+b<-3
+a+b
+for (x in 1:10) {
+  print(x)
+}
+if (a>b) {
+  print("a is greater than b")
+}else{
+  print("b is greater than a") 
+}
+
---
+++ rscode.R
@@ -0,0 +1,9 @@
+"hello world"
+a<-5
+b<-5
+a+b
+name="anil"
+print(a+b)
+for (x in 1:10) {
+  print(x)
+}
